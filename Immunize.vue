9838c8f3bfcb171551597deead04c954d0968ddd
Ran Guin <ran.guin@gmail.com>
2017-08-09 23:52:33 -0700

    remove ImmunizeForm for now

--- Immunize.vue
+++ Immunize.vue
@@ -6,19 +6,16 @@
   div
     button(id="show-modal" @click="showModal = true") Load Table
     dbRecordModal(v-if="showModal" table='user' type='append' @close="showModal=false")
-      h3 custom Header
+    	h3 custom Header
     hr
     FormA(table='Equipment' type='append' prompt='Save Immunization Record')
     hr
     h3 Form Generator Below...
-    ImmunizeForm
 </template>
 
 <script>
-
 import FormA from './../Standard/Form.vue'
 import dbRecordModal from './../Standard/dbRecordModal.vue'
-import ImmunizeForm from './ImmunizeForm.vue'
 
 export default {
   name: 'immunize',
@@ -30,7 +27,7 @@ export default {
   },
   components: {
     FormA,
-    dbRecordModal,
+    dbRecordModal
   },
 
   methods: {
