eefc5f44eced4bebe2dd70eecacd16c0c1c2324f
Ran Guin <ran.guin@gmail.com>
2017-08-09 18:52:16 -0700

    add table spec

--- Patient.vue
+++ Patient.vue
@@ -6,7 +6,7 @@
       button(@click.prevent="clearUser") New Patient
       button(@click.prevent="test") Test
     div(v-if="!patient || !patient.id")
-      Search(scope='patient', model='user' url='http://localhost:1234/Record_API/search', :search="search", prompt='Find Patient')
+      Search(scope='patient', model='user' url='http://localhost:1234/Record_API/search?table=user', :search="search", prompt='Find Patient')
       hr
       Picked(scope='patient')
       hr
