bdbdffe9a640209fb3183a7cc0474f2e054ee708
Ran Guin <ran.guin@gmail.com>
2017-08-10 18:21:40 -0700

    use props instead of store

--- Schedule.vue
+++ Schedule.vue
@@ -1,18 +1,12 @@
 <template lang='pug'>
   div.schedule-section
-    Search(:id='vaccineString' scope='vaccine' method='get' url='https://vids-siv.phac-aspc.gc.ca/api/vaccine.php?' searchParameter='product_name' prompt='Search for Disease of Vaccine')
+    Search(:id='vaccineString' scope='vaccine' method='get' url='https://vids-siv.phac-aspc.gc.ca/api/vaccine.php?' searchParameter='product_name' prompt='Search for Disease of Vaccine' :multiSelect="false")
     hr
-    Picked(scope='vaccine')
-    hr
-    SearchResults(scope='vaccine' reference='gtin')
 
 </template>
 
 <script>
-  import { mapState } from 'vuex'
   import Search from './../Standard/Search.vue'
-  import SearchResults from './../Standard/SearchResults.vue'
-  import Picked from './../Standard/Picked.vue'
 
   export default {
     name: 'schedule',
@@ -22,18 +16,15 @@
         vaccineString: ''
       }
     },
-    components: {
-      Search,
-      SearchResults,
-      Picked
+    props: {
+      patient: {
+        type: Object,
+        default () { return {} }
+      }
     },
-    computed: mapState([
-      'patient',
-      'selected',
-      'searchResults',
-      'searchStatus',
-      'count'
-    ]),
+    components: {
+      Search
+    },
     methods: {
     }
   }
