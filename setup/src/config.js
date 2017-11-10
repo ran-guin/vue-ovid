d76f35f5a8b2f951b0cb516662c3776bd366e661
Ran Guin <ran.guin@gmail.com>
2017-11-10 15:21:26 -0800

    add url to config settings

--- setup/src/config.js
+++ setup/src/config.js
@@ -4,5 +4,9 @@ export default {
   vaccineURL: 'https://vaccineURL',
   userURL: 'http://localhost:1234/Record_API/search?table=user',
   dbURL: 'http://localhost:1234/Record_API/fields',
-  apiUrl: 'http://localhost:3001/'
+  apiUrl: 'https://test-api.herokuapp.com',
+  vaccineSearchURL: 'https://vids-siv.phac-aspc.gc.ca/api/vaccine.php?',
+  diseaseURL: 'http://localhost:1234/Record_API/search?table=disease',
+  travelURL: 'http://localhost:1234/Record_API/search?table=country',
+  countryURL: 'https://restcountries.eu/rest/v2/name/<region>?fields=name;region;subregion'
 }
