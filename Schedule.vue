272de5dee8482c972ec5d325a7757320de06de5f
Ran <ran.guin@gmail.com>
2017-11-16 17:39:35 -0800

    make search more modular

--- Schedule.vue
+++ Schedule.vue
@@ -1,13 +1,26 @@
 <template lang='pug'>
   span.schedule-section
-    Demo(:demo="demo" name='vaccine')
-    Block(title="Scheduled Vaccines" subheader="[available to staff]" :trigger="toggleMe" :data="scheduled" :data_options="data_options" :alt="alt_help")
-    SearchModal(:picked="scheduled" :search_options="search_options" close="Finished adding Immunizations" :toggle="toggle")
+    div.block
+      div.block-header
+        Demo(:demo="demo" name='vaccine')
+        h3 Vaccinations Scheduled
+          SearchModal(id='schedule-modal' type='search' :options="searchOptions" close="Close Scheduler" openButton="+")
+      div.block-body
+        div(v-if="scheduled && scheduled.length")
+          DataGrid.block-grid(:data="scheduled" :data_options="data_options")
+        div(v-else)
+          b Nothing Scheduled
+        div(v-if="help")
+          Block(:content="help") 
+      div.block-footer(v-if="footer")
+        div(v-html="footer")
 </template>
 
 <script>
-  import SearchModal from './../Standard/SearchModal.vue'
+  import SearchModal from './../Standard/Modal.vue'
+  import DataGrid from './../Standard/DataGrid.vue'
   import Block from './../Standard/Block.vue'
+  import SearchBlock from './../Standard/SearchBlock.vue'
   import Demo from './Demo.vue'
   import config from '@/config.js'
   
@@ -15,8 +28,10 @@
     name: 'schedule',
     components: {
       SearchModal,
+      DataGrid,
       Demo,
-      Block
+      Block,
+      SearchBlock
     },
 
     data () {
@@ -32,18 +47,19 @@
             {type: 'button', name: 'more info', modal: {function: this.MoreInfo}}
           ]
         },
-        search_options: {
+        searchOptions: {
           scope: 'vaccine',
+          model: 'vaccine',
           method: 'get',
           url: config.vaccineSearchURL,
           prompt: 'Search Vaccines',
           title: 'Immunizations',
-          field: 'name',
+          field: 'product_name',
           fields: ['product_name', 'din'],
           onPick: this.addCoverage,
           multiSelect: true
         },
-        alt_help: '<b>nothing scheduled</b><p ><UL><LI>Schedule Specific Vaccines for Immunization</LI><LI>Search for Vaccines</LI><LI>Check for Side-effects / Contra-indications</LI></UL>',
+        help: '<p ><UL><LI>Schedule Specific Vaccines for Immunization</LI><LI>Search for Vaccines</LI><LI>Check for Side-effects / Contra-indications</LI></UL>',
         gs: true
       }
     },
@@ -59,6 +75,9 @@
       },
       scheduled: {
         type: Array
+      },
+      footer: {
+        type: String
       }
     },
     methods: {
@@ -72,6 +91,13 @@
       },
       toggleMe: function () {
         this.toggle = !this.toggle
+      }
+    },
+    computed: {
+      options: function () {
+        var searchOptions = this.searchOptions
+        searchOptions.picked = this.scheduled
+        return searchOptions
       }
     }
   }
