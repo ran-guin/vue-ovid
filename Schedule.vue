fbf87b647d5b0c62508a10baa82ee07b4494393f
Ran Guin <ran.guin@gmail.com>
2018-01-23 15:31:59 -0800

    tidy fields; add barcode input option

--- Schedule.vue
+++ Schedule.vue
@@ -3,15 +3,24 @@
     div.block
       div.block-header
         Demo(:demo="demo" name='vaccine')
-        h3 Vaccinations Scheduled
+        h3 Vaccinations Required
           b &nbsp; &nbsp;
-          Modal(id='schedule-modal' type='search' :options="search_modal" :picked="coverage" :onDelete='deleteItem')
+          Modal(id='schedule-modal' type='search' :options="search_modal" :picked="scheduled" :onDelete='deleteItem')
           Modal(id='immunize-modal' type='record' :options="immunize_options" :append="append")
+          span &nbsp; &nbsp; 
+          div.navbar-right
+            div.input-group
+              span.input-group-addon
+                icon(name='barcode')
+              input(type='text' placeholder='-- Scan Barcode --')
+              span.input-group-btn
+                button.btn.btn-primary(type='submit') Apply
+              span &nbsp; &nbsp; 
       div.block-body
         div(v-if="1")
-          DataGrid.block-grid(:options="data_options" :links="links" baseClass='scheduled' fieldClass='status')
+          DataGrid.block-grid(:data="scheduled" :options="data_options" :links="links" baseClass='scheduled' fieldClass='status')
         div(v-else)
-          b Nothing Scheduled
+          b Nothing Required
         div(v-if="help")
           Block(:content="help") 
       div.block-footer(v-if="footer")
@@ -24,6 +33,9 @@
   import Block from './../Standard/Block.vue'
   import SearchBlock from './../Standard/SearchBlock.vue'
   import Demo from './Demo.vue'
+
+  import 'vue-awesome/icons/barcode'
+
   import config from '@/config.js'
   
   export default {
@@ -46,9 +58,9 @@
         links: {
         },
         data_options: {
-          title: 'Scheduled Immunizations',
+          title: 'Pending Immunizations',
           stored: 'coverage',
-          fields: ['vaccine', 'coverage', 'status'],
+          fields: ['vaccine', 'coverage', 'expiry'],
           baseClass: 'scheduled',
           fieldClass: 'status',
           addLinks: [
@@ -68,6 +80,9 @@
           title: 'Immunizations',
           openButton: '+',
           closeButton: 'Close Scheduler',
+          data_options: {
+            fields: ['vaccine', 'coverage', 'expiry']
+          },
           search: {
             scope: 'coverage',
             model: 'coverage',
@@ -76,11 +91,11 @@
             prompt: 'Search Vaccines',
             field: 'vaccine',
             search_fields: ['vaccine', 'code', 'coverage'],
-            show_fields: ['vaccine', 'code', 'coverage', 'status'],
+            show_fields: ['vaccine', 'code', 'coverage', 'expiry'],
             // onPick: this.addCoverage,
             multiSelect: true,
             target: 'coverage',
-            picked: this.coverage,
+            picked: this.scheduled,
             onPick: this.addSchedule,
             stored: 'coverage'
           },
@@ -126,7 +141,7 @@
           staffId = payload.staff.id
 
           this.$set(this.append, 0, {name: 'patient', type: 'hidden', default: patientId})
-          this.$set(this.append, 1, {name: 'vaccinator', type: 'int', default: staffId})
+          this.$set(this.append, 1, {name: 'vaccinator', type: 'hidden', default: staffId})
         }
 
         console.log('MD: ' + JSON.stringify(this.$store.getters.ModalData))
@@ -147,7 +162,7 @@
         // UNNECESSARY ... doesn't need to do anything ...
         console.log('DYNO: add Schedule')
         console.log(JSON.stringify(data))
-        console.log('to: ' + JSON.stringify(this.coverage))
+        console.log('to: ' + JSON.stringify(this.scheduled))
       },
       deleteItem: function (index, key) {
         if (!key) { key = 'coverage' }
@@ -158,27 +173,32 @@
       },
       info: function (record) {
         console.log('retrieve more schedule info from record: ' + JSON.stringify(record))
+        var data = record
+        data.recommendations = '... more details retrieved dynamically via CDC API ...'
+        data.side_effects = '... more details retrieved dynamically via CDC API ...'
+        data.contraindications = '... more details retrieved dynamically via CDC API ...'
+        data.link = '... link to cdc site for more information'
 
-        var data = [
-          {'vaccine': 'Hepatitis B', 'side_effects': 'Pain at the injection site (3%-29%) and a temperature greater than 37.7 C (1%-6%) have been among the most frequently reported side effects among adults and children receiving vaccine (8-12). In placebo-controlled studies, these side effects were reported no more frequently among vaccinees than among persons receiving a placebo (11,12). Among children receiving both hepatitis B vaccine and DTP, these mild side effects have been observed no more frequently than among children receiving only DTP.', 'adverse_effects': 'In the United States, surveillance of adverse reactions indicated a possible association between GBS and receipt of the first dose of plasma-derived hepatitis B vaccine (CDC, unpublished data; 13). However, an estimated 2.5 million adults received one or more doses of recombinant hepatitis B vaccine during 1986-1990, and available data concerning these vaccinees do not indicate an association between receipt of recombinant vaccine and GBS (CDC, unpublished data).\n\nBased on reports to the Vaccine Adverse Events Reporting System (VAERS), the estimated incidence rate of anaphylaxis among vaccine recipients is low (i.e., approximately one event per 600,000 vaccine doses distributed). Two of these adverse events occurred in children (CDC, unpublished data). In addition, only one case of anaphylaxis occurred among 100,763 children ages 10-11 years who had been vaccinated with recombinant vaccine in British Columbia (D. Scheifele, unpublished data), and no adverse events were reported among 166,757 children who had been vaccinated with plasma-derived vaccine in New Zealand (5). Although none of the persons who developed anaphylaxis died, this adverse event can be fatal; in addition, hepatitis B vaccine can -- in rare instances -- cause a life-threatening hypersensitivity reaction in some persons (5). Therefore, subsequent vaccination with hepatitis B vaccine is contraindicated for persons who have previously had an anaphylactic response to a dose of this vaccine.\n\nLarge-scale hepatitis B immunization programs for infants in Alaska, New Zealand, and Taiwan have not established an association between vaccination and the occurrence of other severe adverse events, including seizures and GBS (B. McMahon and A. Milne, unpublished data; 14). However, systematic surveillance for adverse reactions in these populations has been limited, and only a minimal number of children have received recombinant vaccine. Any presumed risk for adverse events that might be causally associated with hepatitis B vaccination must be balanced with the expected risk for hepatitis B virus (HBV)-related liver disease. Currently, an estimated 2,000-5,000 persons in each U.S. birth cohort will die as a result of HBV-related liver disease because of the 5% lifetime risk for HBV infection.\n\nAs hepatitis B vaccine is introduced for routine vaccination of infants, surveillance for vaccine-associated adverse events will continue to be an important part of the program despite the current record of safety. Any adverse event suspected to be associated with hepatitis B vaccination should be reported to VAERS. VAERS forms can be obtained by calling (800) 822-7967'}
-        ]
+        // var data = [
+        //   {'vaccine': 'Hepatitis B', 'side_effects': 'Pain at the injection site (3%-29%) and a temperature greater than 37.7 C (1%-6%) have been among the most frequently reported side effects among adults and children receiving vaccine (8-12). In placebo-controlled studies, these side effects were reported no more frequently among vaccinees than among persons receiving a placebo (11,12). Among children receiving both hepatitis B vaccine and DTP, these mild side effects have been observed no more frequently than among children receiving only DTP.', 'adverse_effects': 'In the United States, surveillance of adverse reactions indicated a possible association between GBS and receipt of the first dose of plasma-derived hepatitis B vaccine (CDC, unpublished data; 13). However, an estimated 2.5 million adults received one or more doses of recombinant hepatitis B vaccine during 1986-1990, and available data concerning these vaccinees do not indicate an association between receipt of recombinant vaccine and GBS (CDC, unpublished data).\n\nBased on reports to the Vaccine Adverse Events Reporting System (VAERS), the estimated incidence rate of anaphylaxis among vaccine recipients is low (i.e., approximately one event per 600,000 vaccine doses distributed). Two of these adverse events occurred in children (CDC, unpublished data). In addition, only one case of anaphylaxis occurred among 100,763 children ages 10-11 years who had been vaccinated with recombinant vaccine in British Columbia (D. Scheifele, unpublished data), and no adverse events were reported among 166,757 children who had been vaccinated with plasma-derived vaccine in New Zealand (5). Although none of the persons who developed anaphylaxis died, this adverse event can be fatal; in addition, hepatitis B vaccine can -- in rare instances -- cause a life-threatening hypersensitivity reaction in some persons (5). Therefore, subsequent vaccination with hepatitis B vaccine is contraindicated for persons who have previously had an anaphylactic response to a dose of this vaccine.\n\nLarge-scale hepatitis B immunization programs for infants in Alaska, New Zealand, and Taiwan have not established an association between vaccination and the occurrence of other severe adverse events, including seizures and GBS (B. McMahon and A. Milne, unpublished data; 14). However, systematic surveillance for adverse reactions in these populations has been limited, and only a minimal number of children have received recombinant vaccine. Any presumed risk for adverse events that might be causally associated with hepatitis B vaccination must be balanced with the expected risk for hepatitis B virus (HBV)-related liver disease. Currently, an estimated 2,000-5,000 persons in each U.S. birth cohort will die as a result of HBV-related liver disease because of the 5% lifetime risk for HBV infection.\n\nAs hepatitis B vaccine is introduced for routine vaccination of infants, surveillance for vaccine-associated adverse events will continue to be an important part of the program despite the current record of safety. Any adverse event suspected to be associated with hepatitis B vaccination should be reported to VAERS. VAERS forms can be obtained by calling (800) 822-7967'}
+        // ]
 
-        this.$store.dispatch('setModalData', data)
+        this.$store.dispatch('setModalData', data, {spin: true})
         this.$store.getters.toggleModal('info-modal')
       }
     },
     computed: {
-      coverage: function () {
+      scheduled: function () {
         var C = this.$store.getters.getHash('coverage')
         var S = []
-        // console.log('loaded Coverage: ' + JSON.stringify(C))
-        // for (var i = 0; i < C.length; i++) {
-        //   if (C[i].status && C[i].status === 'covered') {
-        //     S.push(C[i])
-        //   }
-        // }
+        console.log('loaded Coverage: ' + JSON.stringify(C))
+        for (var i = 0; i < C.length; i++) {
+          if (C[i].status && !C[i].status.match(/covered|expired/)) {
+            S.push(C[i])
+          }
+        }
         console.log('loaded Schedule:' + JSON.stringify(S))
-        return C
+        return S
       },
       updates: function () {
         var updates = this.$store.getters.getHash('updates')
@@ -187,7 +207,7 @@
       },
       options: function () {
         var searchOptions = this.searchOptions
-        searchOptions.picked = this.coverage
+        searchOptions.picked = this.scheduled
         return searchOptions
       }
     }
@@ -204,11 +224,14 @@
     padding: 10px;
   }
   .scheduled.covered {
-    background-color: red;
+    background-color: lightgreen;
     /*display: none;*/
   }
   .scheduled.due {
     background-color: yellow;
+  }
+  .scheduled.expiring {
+    background-color: pink;
   }
   .scheduled.recommended {
     background-color: yellow;
