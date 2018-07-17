573c4acc49da1a68558dd7ced359b7081f63af29
Ran Guin <ran.guin@gmail.com>
2018-07-17 13:01:04 -0700

    minor updates (?)

--- PrivateHeader.vue
+++ PrivateHeader.vue
@@ -1,25 +1,24 @@
 <template lang='pug'>
   div.info-header
-    div.col-md-2.info-logo
+    div.col-xs-2.info-logo
       <!-- a(href='/ovid') -->
         router-link(:to="{name: 'About Ovid'}")
-          img(src='/static/images/needleIcon.png' height='40px')
-    div.col-md-6.info-left
+          img(src='/static/images/needleIcon.png' height='40px' data-tooltip title='About Ovid')
+    div.col-xs-6.info-left
       b.input-lg OVID
-      
+      b.full-page &nbsp; - &nbsp; Online Vaccination & Immunization Database
 
       <!-- Demo(:demo="demo" name="patient") -->
       <!-- User(role='patient' :user="patient" title='Patient' :globalSearch="userSearch" :fields="userFields") -->
-    div.col-md-4
+    div.col-xs-4
       <!-- navbar-right items listed from right to left ! -->
-      span.navbar-right
-        Popup(icon='bars')
-      span.navbar-right
-        b.input-lg User: 
-          span(v-if="payload && payload.staff")
-            b {{payload.staff.name}}
-          span(v-else)
-            b Demo Admin
+      span
+        table.table
+          tr
+            td
+              SearchButton()
+          <!-- td -->
+            <!-- LoginPopup(icon='bars') &nbsp; &nbsp;  -->
 
         <!-- Demo(:demo="demo" name='staff') -->
         <!-- User(role='staff' :user="staff" title='staff' include='staff' :globalSearch="staffSearch" :search="addStaff") -->
@@ -28,7 +27,8 @@
 <script>
   import User from '@/components/User'
   import Demo from '@/components/ovid/Demo'
-  import Popup from '@/components/Standard/Popup'
+  // import LoginPopup from '@/components/Standard/LoginPopup'
+  import SearchButton from '@/components/Standard/SearchButton'
 
   import 'vue-awesome/icons/home'
   import 'vue-awesome/icons/bars'
@@ -45,7 +45,7 @@
     components: {
       User,
       Demo,
-      Popup
+      SearchButton
     },
     props: {
       searchOpen: {
@@ -111,6 +111,12 @@
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+
+@media (max-width: 900px) {
+  .full-page {
+    display: none;
+  }
+}
 
 .info-header {
   padding: 10px;
